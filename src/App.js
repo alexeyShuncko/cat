bcacbbf132f5293914add7a54694c636e2cf3e8f
alexeyShuncko <shuncko.a@yandex.ru>
2023-01-19 16:58:45 +0300

    Логика выбора карточки

--- src/App.js
+++ src/App.js
@@ -1,14 +1,66 @@
+import { useState } from 'react';
 import s from './App.module.css';
 import Card from './components/Card';
 
 function App() {
+  const [data, setData] = useState([
+    {
+      filling: 'с фуа-гра',
+      count: {
+        portion: '10 порций',
+        gift: 'мышь в подарок',
+        other: '',
+      },
+      weight: '0,5',
+      description: 'Печень утки разварная с артишоками.',
+      status: false,
+    },
+    {
+      filling: 'с рыбой',
+      count: {
+        portion: '40 порций',
+        gift: '2 мыши в подарок',
+        other: '',
+      },
+      weight: '2',
+      description: 'Головы щучьи с чесноком да свежайшая сёмгушка.',
+      status: false,
+    },
+    {
+      filling: 'с курой',
+      count: {
+        portion: '100 порций',
+        gift: '5 мышей в подарок',
+        other: 'заказчик доволен',
+      },
+      weight: '5',
+      description: 'Филе цыплят с трюфелями в бульоне.',
+      status: false,
+    },
+  ]);
+
+  console.log(data[0]);
   return (
     <div className={s.container}>
       <div className={s.title}>Ты сегодня покормил кота?</div>
       <div className={s.cardList}>
-        <Card />
-        <Card />
-        <Card />
+        {data.map((a, i) => (
+          <div
+            key={a.description}
+            onClick={() => {
+              a.status = !a.status;
+              setData([...data]);
+            }}>
+            <Card data={data[i]} />
+            {a.status === false ? (
+              <div className={s.btn}>
+                Чего сидишь? Порадуй котэ, <span>купи.</span>
+              </div>
+            ) : (
+              <div className={s.btn}>{a.description}</div>
+            )}
+          </div>
+        ))}
       </div>
     </div>
   );
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,27 +1,27 @@
 import s from './Card.module.css';
 import cat from '../images/Photo.png';
 
-const Card = () => {
+const Card = ({ data }) => {
+  let color = data.status === false ? '#1698D9' : '#D91667';
+
   return (
     <div className={s.cardContainer}>
-      <div className={s.card}>
+      <div className={s.card} style={{ borderColor: color }}>
         <div className={s.subTitle}>Сказачное заморское яство</div>
         <div className={s.title}>Нямушка</div>
-        <div className={s.filling}>с фуа-гра</div>
+        <div className={s.filling}>{data.filling}</div>
         <div className={s.count}>
-          <div>10 порций</div>
-          <div>мышь в подарок</div>
+          <div>{data.count.portion}</div>
+          <div>{data.count.gift}</div>
+          {data.count.other !== '' && <div>{data.count.other}</div>}
         </div>
         <img src={cat} alt="Cat" className={s.catImg} />
-        <div className={s.circl}>
-          <span>0,5</span>
+        <div className={s.circl} style={{ background: color }}>
+          <span>{data.weight}</span>
           <span>кг</span>
         </div>
       </div>
-      <div className={s.btn}>
-        Чего сидишь? Порадуй котэ, <span>купи.</span>
-      </div>
-      <span className={s.hiddenEl}></span>
+      <span className={s.hiddenEl} style={{ borderColor: color }}></span>
     </div>
   );
 };
